500531a097b5c0f8015ad273071e7c00bb1e1465
samahahmed115 <samahahmeddd115@gmail.com>
2025-09-07 21:53:16 +0300

    add project files

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import TodoForm from './components/TododForm';
+
+
+
+
+function App(){
+    return(
+        <div>
+            <TodoForm/>
+        </div>
+    )
+}
+
+export default App;
+
+
+
+
